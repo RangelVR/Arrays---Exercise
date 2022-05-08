4bf1ab110cead4e9cfa1241b92264cf816dda931
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-08 20:51:35 +0300

    Add files via upload

---
+++ 01. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vagons = int.Parse(Console.ReadLine());
+            int[] allPassanger = new int[vagons];
+            int sum = 0;
+
+            for (int i = 0; i < vagons; i++)
+            {
+                int passangers = int.Parse(Console.ReadLine());
+                allPassanger[i] = passangers;
+                sum += passangers;
+            }
+
+            Console.WriteLine(string.Join(" ", allPassanger));
+            Console.WriteLine(sum);
+        }
+    }
+}
