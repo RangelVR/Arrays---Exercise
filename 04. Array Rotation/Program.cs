29ee8a88779cc172c762991e7212bc5d421418e9
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-10 22:54:52 +0300

    Add files via upload

---
+++ 04. Array Rotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n % arr.Length; i++)
+            {
+                string firstIndex = arr[0];
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length - 1] = firstIndex;
+                
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
