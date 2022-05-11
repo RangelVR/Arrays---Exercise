dd609e67146f1cf1099e0b8c64d21dedde76a7c8
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-12 00:20:04 +0300

    Add files via upload

---
+++ 05. Top Integers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            string topIntgeres = string.Empty;
+
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                bool flag = false;
+                string topInt = string.Empty;
+               
+                for (int j = i; j < input.Length - 1; j++)
+                {
+                    if (input[i] > input[j + 1])
+                    {
+                        topInt = input[i].ToString();
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    topIntgeres += topInt;
+                    topIntgeres += " ";
+                }
+
+            }
+            topIntgeres += input[input.Length - 1];
+
+            Console.WriteLine(topIntgeres.Trim());
+        }
+    }
+}
