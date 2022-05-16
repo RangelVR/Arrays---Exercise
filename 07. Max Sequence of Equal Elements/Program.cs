4e1ce4adf4048b5eea31dd130893a5aacd2a1332
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-17 00:21:57 +0300

    Add files via upload

---
+++ 07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = 0;
+            int max = 0;
+            int value = 0;
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+
+                if (arr[i] == arr[i - 1])
+                {
+                    counter++;
+                    if (counter >= max)
+                    {
+                        value = arr[i];
+                        max = counter;
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+
+            for (int i = 0; i <= max; i++)
+            {
+                Console.Write($"{value} ");
+            }
+
+        }
+    }
+}
