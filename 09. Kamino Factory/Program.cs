65605ba4bc7124c695df0c7f4c9cb7c762723887
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-19 23:25:29 +0300

    Add files via upload

---
+++ 09. Kamino Factory/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Kamino_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int[] dnaSequens = new int[lenght];
+            
+            int semple = 0;
+            int max = 0;
+            int sum = 0;
+            int maxSum = 0;
+            
+
+
+            while (input != "Clone them!")
+            {
+                int[] arr = input
+                    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int counter = 0;
+                bool isFound = false;
+
+                sum = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    sum += arr[j];
+
+                }
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+
+                    if (sum > maxSum)
+                    {
+                        if (arr[i] == arr[i + 1])
+                        {
+                            counter++;
+                            if (counter >= max)
+                            {
+                                counter++;
+                                max = counter;
+                                maxSum = sum;
+                                dnaSequens = arr;
+                                isFound = true;
+                            }
+
+                        }
+                    }
+                    
+                }
+                
+
+                if (isFound)
+                {
+                    semple++;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Best DNA sample {semple} with sum: {maxSum}.");
+            Console.WriteLine(string.Join(" ", dnaSequens));
+        }
+    }
+}
