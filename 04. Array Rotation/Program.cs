550dad75879c6a630cab5735daf4be51cba81a3b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-27 22:42:51 +0300

    Update Program.cs

--- 04. Array Rotation/Program.cs
+++ 04. Array Rotation/Program.cs
@@ -1,28 +1,24 @@
-﻿using System;
+using System;
 using System.Linq;
 
-namespace _04._Array_Rotation
+namespace MaxSequenceOfEqualElements
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] arr = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .ToArray();
-            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < n % arr.Length; i++)
+            for (int i = 0; i < rotations % arr.Length; i++)
             {
-                string firstIndex = arr[0];
+                int firstIndex = arr[0];
                 for (int j = 0; j < arr.Length - 1; j++)
                 {
                     arr[j] = arr[j + 1];
                 }
                 arr[arr.Length - 1] = firstIndex;
-                
             }
-
             Console.WriteLine(string.Join(" ", arr));
         }
     }
