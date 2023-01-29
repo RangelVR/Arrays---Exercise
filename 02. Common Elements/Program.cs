9fef314078a540601df4403c71a0f29413020b45
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 14:25:30 +0200

    Update Program.cs

--- 02. Common Elements/Program.cs
+++ 02. Common Elements/Program.cs
@@ -1,37 +1,26 @@
-using System;
-using System.Linq;
+string[] arr1 = Console.ReadLine().Split().ToArray();
+string[] arr2 = Console.ReadLine().Split().ToArray();
 
-namespace MaxSequenceOfEqualElements
+
+foreach (var itemInArr2 in arr2)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string[] arr1 = Console.ReadLine().Split().ToArray();
-            string[] arr2 = Console.ReadLine().Split().ToArray();
-
-            foreach (var item2 in arr2)
-            {
-                foreach (var item1 in arr1)
-                {
-                    if (item2 == item1)
-                    {
-                        Console.Write(item2 + " ");
-                    }
-                }
-            }
-
-            //for (int i = 0; i < arr2.Length; i++)
-            //{
-            //    for (int j = 0; j < arr1.Length; j++)
-            //    {
-            //        if (arr2[i] == arr1[j])
-            //        {
-            //            Console.Write(arr2[i] + " ");
-            //        }
-            //    }
-            //  
-            //}
-        }
-    }
+	foreach (var itemInArr1 in arr1)
+	{
+		if (itemInArr1 == itemInArr2)
+		{
+			Console.Write(itemInArr2 + " ");
+		}
+	}
 }
+
+
+//for (int i = 0; i < arr2.Length; i++)
+//{
+//	for (int k = 0; k < arr1.Length; k++)
+//	{
+//		if (arr2[i] == arr1[k])
+//		{
+//			Console.Write(arr2[i] + " ");
+//		}
+//	}
+//}
