69b9890729c76e5c6de7b76bbfffc470a82519b6
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 15:25:49 +0200

    Update Program.cs

--- 03. Zig-Zag Arrays/Program.cs
+++ 03. Zig-Zag Arrays/Program.cs
@@ -1,32 +1,26 @@
-using System;
-using System.Linq;
+int n = int.Parse(Console.ReadLine());
 
-namespace MaxSequenceOfEqualElements
+int[] arr1 = new int[n];
+int[] arr2 = new int[n];
+
+
+for (int i = 0; i < n; i++) 
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] arr1 = new int[n];
-            int[] arr2 = new int[n];
+    int[] inputArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            for (int i = 0; i < n; i++)
-            {
-                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                if (i % 2 == 0)
-                {
-                    arr1[i] = arr[0];
-                    arr2[i] = arr[1];
-                }
-                else
-                {
-                    arr1[i] = arr[1];
-                    arr2[i] = arr[0];
-                }
-            }
-            Console.WriteLine(string.Join(" ", arr1));
-            Console.WriteLine(string.Join(" ", arr2));
-        }
+
+    if (i % 2 == 0)
+    {
+        arr1[i] = inputArr[0];
+        arr2[i] = inputArr[1];
     }
+    else
+    {
+        arr1[i] = inputArr[1];
+        arr2[i] = inputArr[0];
+    }
+
 }
+
+Console.WriteLine(string.Join(" ", arr1));
+Console.WriteLine(string.Join(" ", arr2));
