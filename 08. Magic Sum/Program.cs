e08ecf9fe855155d010847d50a89bf8dff5e7e7d
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 20:02:12 +0200

    Update Program.cs

--- 08. Magic Sum/Program.cs
+++ 08. Magic Sum/Program.cs
@@ -1,25 +1,13 @@
-using System;
-using System.Linq;
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
 
-namespace MaxSequenceOfEqualElements
+for(int i = 0; i < arr.Length; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int magicNum = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int k = i + 1; k < arr.Length; k++)
-                {
-                    if (arr[i] + arr[k] == magicNum)
-                    {
-                        Console.WriteLine($"{arr[i]} {arr[k]}");
-                    }
-                }
-            }
-        }
-    }
+	for (int k = i; k < arr.Length - 1; k++)
+	{
+		if (arr[i] + arr[k + 1] == n)
+		{
+			Console.WriteLine($"{arr[i]} {arr[k + 1]}");
+		}
+	}
 }
