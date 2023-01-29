0802043f059061bcc642b4075763f0f6843a0c90
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 19:04:23 +0200

    Update Program.cs

--- 06. Equal Sum/Program.cs
+++ 06. Equal Sum/Program.cs
@@ -1,41 +1,35 @@
-using System;
-using System.Linq;
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-namespace MaxSequenceOfEqualElements
+int rightSum = 0;
+int leftSum = 0;
+int index = 0;
+
+for (int i = 0; i < arr.Length; i++)
 {
-    class Program
+    rightSum = 0;
+    for (int j = 0; j < i; j++)
     {
-        static void Main(string[] args)
-        {
-            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            bool foundIndex = false;
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                int leftSum = 0;
-                for (int j = 0; j < i; j++)
-                {
-                    leftSum += arr[j];
-                }
-
-                int rightSum = 0;
-                for (int k = i + 1; k < arr.Length; k++)
-                {
-                    rightSum += arr[k];
-                }
-
-                if (leftSum == rightSum)
-                {
-                    Console.WriteLine(i);
-                    foundIndex = true;
-                    break;
-                }
-            }
-            if (!foundIndex)
-            {
-                Console.WriteLine("no");
-            }
-
-        }
+        rightSum += arr[j];
     }
+
+    leftSum = 0;
+    for (int k = i + 1; k < arr.Length - 1 + 1; k++)
+    {
+        leftSum += arr[k];
+    }
+
+    if (rightSum == leftSum)
+    {
+        index = i;
+        break;
+    }
+}
+
+if (rightSum == leftSum)
+{
+    Console.WriteLine(index);
+}
+else
+{
+    Console.WriteLine("no");
 }
