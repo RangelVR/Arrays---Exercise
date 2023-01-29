87892f08e78c9d2170a1d7e04d7473cc156947d5
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 20:03:51 +0200

    Update Program.cs

--- 08. Magic Sum/Program.cs
+++ 08. Magic Sum/Program.cs
@@ -1,13 +1,13 @@
 int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-int n = int.Parse(Console.ReadLine());
+int magicNum = int.Parse(Console.ReadLine());
 
-for(int i = 0; i < arr.Length; i++)
+for (int i = 0; i < arr.Length; i++)
 {
-	for (int k = i; k < arr.Length - 1; k++)
-	{
-		if (arr[i] + arr[k + 1] == n)
-		{
-			Console.WriteLine($"{arr[i]} {arr[k + 1]}");
-		}
-	}
+    for (int k = i; k < arr.Length - 1; k++)
+    {
+        if (arr[i] + arr[k + 1] == magicNum)
+        {
+            Console.WriteLine($"{arr[i]} {arr[k + 1]}");
+        }
+    }
 }
