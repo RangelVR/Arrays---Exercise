3876bbc422892fe6e98406441e169970d3f258d5
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 14:11:24 +0200

    Update Program.cs

--- 01. Train/Program.cs
+++ 01. Train/Program.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Linq;
+int wagons = int.Parse(Console.ReadLine());
+int[] train = new int[wagons];
 
-namespace MaxSequenceOfEqualElements
+for (int i = 0; i < wagons; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int wagons = int.Parse(Console.ReadLine());
-            int[] train = new int[wagons];
-
-            for (int i = 0; i < wagons; i++)
-            {
-                train[i] = int.Parse(Console.ReadLine());
-            }
-            Console.WriteLine(string.Join(" ", train));
-            Console.WriteLine(string.Join(" ", train.Sum()));
-        }
-    }
+    train[i] = int.Parse(Console.ReadLine());
 }
+
+Console.WriteLine(string.Join(" ", train));
+int totalPassangers = train.Sum();
+Console.WriteLine($"{totalPassangers}");
+
+//Console.WriteLine(string.Join(" ", train));
+//Console.WriteLine(train.Sum());
