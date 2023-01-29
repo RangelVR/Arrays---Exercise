88e3252b91f05b5f86c78a95914311b5d6353c5a
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-29 19:43:03 +0200

    Update Program.cs

--- 07. Max Sequence of Equal Elements/Program.cs
+++ 07. Max Sequence of Equal Elements/Program.cs
@@ -1,37 +1,30 @@
-using System;
-using System.Linq;
 
-namespace MaxSequenceOfEqualElements
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int counter = 0;
+int maxSequence = int.MinValue;
+int numberToRepeat = 0;	
+
+for (int i = 0; i < arr.Length - 1; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int seqCount = 0;
-            int counter = 0;
-            int n = 0;
 
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                if (arr[i] == arr[i + 1])
-                {
-                    counter++;
-                    if (counter > seqCount)
-                    {
-                        n = arr[i];
-                        seqCount = counter;
-                    }
-                }
-                else
-                {
-                    counter = 0;
-                }
-            }
-            for (int i = 0; i <= seqCount; i++)
-            {
-                Console.Write(n + " ");
-            }
-        }
-    }
+    if (arr[i] == arr[i + 1])
+	{
+		counter++;
+
+		if (counter > maxSequence)
+		{
+			maxSequence = counter;
+			numberToRepeat = arr[i];
+		}
+	}
+	else
+	{
+		counter = 0;
+	}
+}
+
+for (int i = 0; i <= maxSequence; i++)
+{
+	Console.Write(numberToRepeat + " ");
 }
